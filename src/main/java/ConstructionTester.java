dcb8564553753bd123e08dffd4529e589ae6bc06
IJaamesI <james.2443235@nv.ccsd.net>
2021-10-15 13:58:54 -0700

    Finished ConstructionTester and HowFarAway using Construction and GeoLocation.

--- src/main/java/ConstructionTester.java
+++ src/main/java/ConstructionTester.java
@@ -6,5 +6,34 @@ public class ConstructionTester
     public static void main(String[] args)
     {
         // Start here!
+        // Scanner for input
+        Scanner scanner = new Scanner(System.in);
+
+        // variables for user inputs
+        double taxRate;
+        double boardCost = 8;
+        double windowCost = 11;
+        int numBoards;
+        int numWindows;
+
+        // asking user for input and setting values
+        System.out.println("Enter the sales tax rate: ");
+        taxRate = scanner.nextDouble();
+        System.out.println("How many boards do you need? ");
+        numBoards = scanner.nextInt();
+        System.out.println("How many windows do you need? ");
+        numWindows = scanner.nextInt();
+
+
+        // calling Construction class and set values
+        Construction set = new Construction(boardCost, windowCost, taxRate);
+
+        // finding total
+        double total = set.lumberCost(numBoards) + set.windowCost(numWindows);
+        System.out.println("Total: " + total);
+        // finding grand total
+        System.out.print("Grand Total: " + (total + (total * set.taxRate)));
+        // close scanner
+        scanner.close();
     }
 }
--- src/main/java/HowFarAway.java
+++ src/main/java/HowFarAway.java
@@ -6,5 +6,37 @@ public class HowFarAway
     public static void main(String[] args)
     {
         // your code here.
+        // create and open scanner
+        Scanner scanner = new Scanner(System.in);
+
+        // create variables
+        double latitudeStart;
+        double longitudeStart;
+        double latitudeEnd;
+        double longitudeEnd;
+
+        // get inputs
+        System.out.println("Enter the latitude of the starting location: ");
+        latitudeStart = scanner.nextDouble();
+        System.out.println("Enter the longitude of the starting location: ");
+        longitudeStart = scanner.nextDouble();
+        System.out.println("Enter the latitude of the ending location: ");
+        latitudeEnd = scanner.nextDouble();
+        System.out.println("Enter the longitude of the ending location: ");
+        longitudeEnd = scanner.nextDouble();
+
+        // make GeoLocations for start and end
+        GeoLocation pos1 = new GeoLocation(latitudeStart, longitudeStart);
+        GeoLocation pos2 = new GeoLocation(latitudeEnd, longitudeEnd);
+
+        // go find distance between the two
+        double result = pos1.distanceFrom(pos2);
+
+        // return distance
+        System.out.print("The distance is " + result + " miles.");
+
+
+        // close scanner
+        scanner.close();
     }
 }
